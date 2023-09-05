febf5804c89e7d7ba7b51ea7280acdc06ce6e388
koromierik <1erikmark@gmail.com>
2023-09-05 12:07:28 +0200

    Kezdés4

--- src/App.java
+++ src/App.java
@@ -1,16 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-       Maci maci = new Maci();
-       Cica mici = new Cica();
-       Pingvin bucu = new Pingvin(); 
-       maci.hangotad("brumm..");
-       maci.mezgyujtes(); 
- ///   maci.nev = "Brumi";
-       System.out.println(mici.nev);
-       mici.hangotad("meow..");
-       mici.dorombolas();
- ///   mici.nev = "";    
-       bucu.hangotad("oaaaa...");
-       bucu.csuszkalas();
-    }
+      Kert kert = new Kert();
+      kert.inditElet();
+
+      }
 }
---
+++ src/Kert.java
@@ -0,0 +1,20 @@
+public class Kert {
+    public void inditElet(){
+        
+        Maci maci = new Maci();
+        Cica mici = new Cica();
+        Pingvin bucu = new Pingvin(); 
+        maci.hangotad("brumm..");
+        maci.mezgyujtes(); 
+  ///   maci.nev = "Brumi";
+       
+        mici.hangotad("meow..");
+        mici.dorombolas();
+  ///   mici.nev = "";    
+       
+        bucu.hangotad("oaaaa...");
+        bucu.csuszkalas();
+     
+    }
+ }
+ 
